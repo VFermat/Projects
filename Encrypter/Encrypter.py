f946d1028e4e8df5dbdad0798a4b35594c0ebcaa
VFermat <vitorge@al.insper.edu.br>
2018-10-09 13:45:17 -0300

    Defined which encrypters we will create

--- Encrypter/Encrypter.py
+++ Encrypter/Encrypter.py
@@ -1,4 +1,25 @@
 class Encrypter:
 
-    def  __init__(self):
+    def __init__(self):
+        pass
+
+    def encrypt(self, message, type='ceasar'):
+        pass
+
+    def _ceasar_encryption(self):
+        pass
+
+    def _ceasar_decryption(self, key):
+        pass
+
+    def _vernam_encryption(self):
+        pass
+
+    def _vernam_decryption(self, key):
+        pass
+
+    def _vigenere_encryption(self):
+        pass
+
+    def _vigenere_decryption(self, key):
         pass
