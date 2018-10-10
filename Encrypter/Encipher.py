105adc1948f019b45a1bac4c7c19ba86add7d06a
VFermat <vitorge@al.insper.edu.br>
2018-10-10 08:56:46 -0300

    Added Affine Encryption and functions needed to create a random key for it

--- Encrypter/Encipher.py
+++ Encrypter/Encipher.py
@@ -41,6 +41,7 @@ class Encipher:
                 print('Invalid Key. Algorithm is creating a random key for you!')
                 key = random.randint(0, 25)
                 print('Your key is {}'.format(key))
+        # Breaking the message into characters
         word = [m for m in self.encrypted_message if m in self.alphabet]
         for i in range(len(word)):
             # Position of the letter on the alphabet
@@ -51,7 +52,6 @@ class Encipher:
             encrypted_message += self.alphabet[e]
         self.encrypted_message = encrypted_message
         return encrypted_message
-
 
     def _ceasar_decryption(self, key):
         decrypted_message = ''
@@ -66,10 +66,29 @@ class Encipher:
         self.decrypted_message = decrypted_message
         return self.decrypted_message
 
-    def _vernam_encryption(self):
-        pass
+    def _affine_encryption(self, a_key, b_key):
+        if type(a_key) is not int:
+            print('Invalid a key. We are creating a random key for you.')
+            a_key = self._random_prime()
+            print('a key: {}'.format(a_key))
+        if type(b_key) is not int:
+            print('Invalid b key. We are creating a random key for you.')
+            b_key = random.randint(1, 11)
+            print('b key: {}'.format(b_key))
+        encrypted_message = ''
+        # Breaking the message into characters
+        word = [m for m in self.encrypted_message if m in self.alphabet]
+        for i in range(len(word)):
+            # Position of the letter on the alphabet
+            m = self.alphabet.index(word[i])
+            # Position of the encrypted letter on the alphabet
+            e = (a_key*m + b_key) % len(self.alphabet)
+            # Adding letter to the encrypted message
+            encrypted_message += self.alphabet[e]
+        self.encrypted_message = encrypted_message
+        return encrypted_message
 
-    def _vernam_decryption(self, key):
+    def _affine_decryption(self, key):
         pass
 
     def _vigenere_encryption(self, key):
@@ -113,3 +132,21 @@ class Encipher:
             decrypted_message += self.alphabet[d]
         self.decrypted_message = decrypted_message
         return decrypted_message
+
+    def _random_prime(self):
+        primes = self._get_primes(30)
+        rand = random.randint(0, len(primes) - 1)
+        return primes[rand]
+
+    def _get_primes(self, limit):
+        primes = []
+        for i in range(limit):
+            if self._is_prime(i):
+                primes.append(i)
+        return primes
+
+    def _is_prime(self, i):
+        for divisor in range(2, i-1):
+            if i % divisor == 0:
+                return False
+        return True
